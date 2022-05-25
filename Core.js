4be42b546eb206cac12038edd3772ff258940bdc
Ershad Raoufi <waveershad21@gmail.com>
2022-05-25 21:56:51 +0430

    Add Wainting and Proccesing

--- Core.js
+++ Core.js
@@ -1,17 +1,24 @@
+
+const Questions = ["FirstName","LastName","NicName","Age","BirthDate","Pet's Name"];
+const Answers = [];
 class PasswordList {
-
-    constructor() {
-
-    }
-    GetPasswordList() {
-        return "GetPasswordList";
-    }
+    constructor() {}
+    GetPasswordList() {}
 }
 var NextCounter=0;
 function ProccessAnswers(){
     console.log(new PasswordList().GetPasswordList())
 }
-
 function GetQuestions(){
-    return ["FirstName","LastName","NicName","Age","BirthDate","Pet's Name"]
+    return Questions;
+}
+function Next_op() {
+    Answers.push(document.getElementById(Questions[NextCounter]+"_id").value);
+    
+    if (NextCounter+1 == GetQuestions().length) {
+        document.getElementById("qestions").setAttribute("class", "d-none");
+        document.getElementById("loading").setAttribute("class", "row center");
+        console.log("End of Qeustion");
+    }
+    ++NextCounter;
 }
